d5b11d6d707629ef7ae0b0ca92927b25323ace9c
fernando <oliveira.desenv@gmail.com>
2026-04-17 10:31:09 -0300

    Projeto auth plataform

--- auth-service/src/main/java/com/authplatform/auth/AuthServiceApplication.java
+++ auth-service/src/main/java/com/authplatform/auth/AuthServiceApplication.java
@@ -3,10 +3,8 @@ package com.authplatform.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableConfigurationProperties
 public class AuthServiceApplication {
 
--- authorization-service/src/main/java/com/authplatform/authorization/domain/port/in/CreateRoleCommand.java
+++ authorization-service/src/main/java/com/authplatform/authorization/domain/port/in/CreateRoleCommand.java
@@ -1,10 +1,7 @@
 package com.authplatform.authorization.domain.port.in;
 
-import java.util.List;
-
 public record CreateRoleCommand(
     String name,
     String description,
-    String applicationId,
-    List<String> permissionIds
+    String applicationId
 ) {}
